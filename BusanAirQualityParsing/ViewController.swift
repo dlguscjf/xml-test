073abb3f3e9e234234e6ad154e339606f4026885
Jonghyun Kim <jhkim@dit.ac.kr>
2018-09-15 23:30:00 +0900

    1st commit

--- BusanAirQualityParsing/ViewController.swift
+++ BusanAirQualityParsing/ViewController.swift
@@ -8,18 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, XMLParserDelegate {
+    
+    var items = [AirQuailtyData]()
+    var item = AirQuailtyData()
+    var myPm10 = ""
+    var myPm25 = ""
+    var mySite = ""
+    var currentElement = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let key = "aT2qqrDmCzPVVXR6EFs6I50LZTIvvDrlvDKekAv9ltv9dbO%2F8i8JBz2wsrkpr9yrPEODkcXYzAqAEX1m%2Fl4nHQ%3D%3D"
+        
+        let strURL = "http://opendata.busan.go.kr/openapi/service/AirQualityInfoService/getAirQualityInfoClassifiedByStation?ServiceKey=\(key)&Date_hour=2018091520&numOfRows=21"
+        
+        let url = URL(string: strURL)
+        print(url!)
+        
+        if let url = URL(string: strURL) {
+            if let parser = XMLParser(contentsOf: url) {
+                parser.delegate = self
+                
+                if (parser.parse()) {
+                    print("parsing success")
+//                    print(items[0].dPm10)
+                    print("PM 10 in Busan")
+                    for i in 0..<items.count {
+                        print("\(items[i].dSite) : \(items[i].dPm10)")
+                    }
+                    
+                } else {
+                    print("parsing fail")
+                }
+            } else {
+                print("url error")
+            }
+        }
+        
+    }
+    
+    // XML Parser Delegate
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        currentElement = elementName
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        let data = string.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
+        if !data.isEmpty {
+//            print("data = \(data)")
+            switch currentElement {
+                case "pm10" : myPm10 = data
+                case "pm25" : myPm25 = data
+                case "site" : mySite = data
+                default : break
+            }
+        }
     }
-
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "item" {
+            let myItem = AirQuailtyData()
+            myItem.dPm10 = myPm10
+            myItem.dPm25 = myPm25
+            myItem.dSite = mySite
+            items.append(myItem)
+        }
+    }
 
 }
 
